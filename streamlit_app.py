892a759c9ae4b3ca23dff063f271a2c5aaa12c73
ncode4ever <137863899+ncode4ever@users.noreply.github.com>
2023-06-27 19:31:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 
+streamlit.title('New Healthy Diner !!')
+
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
