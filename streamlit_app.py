ec11277daf81fc1e603689ffa0a791e8d711ebe9
ncode4ever <137863899+ncode4ever@users.noreply.github.com>
2023-06-28 23:43:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 import pandas
+import requests
+import snowflake.connector
 
 streamlit.title('New Healthy Diner !!')
 
@@ -21,7 +23,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-import requests
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
@@ -30,7 +31,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+# stop the streamlit
+streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
@@ -38,9 +41,10 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("Fruit Loadlist Contains :")
 streamlit.text(my_data_row)
-# streamlit.dataframe(my_data_row)
 
 # Add a textbox and display selected fruit
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 # add_my_fruit = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Jackfruit'])
-streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('Thanks for adding : ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list('from streamlit')")
